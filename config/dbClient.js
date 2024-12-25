7bd42939d4790d829ac3f35b5c2dc59c4396b6af
GeronimoDavis <geronimodavis@gmail.com>
2024-12-25 19:37:20 -0300

    usando mongoose

--- config/dbClient.js
+++ config/dbClient.js
@@ -1,23 +1,28 @@
 import "dotenv/config";
 import { MongoClient } from "mongodb";
+import mongoose, { mongo } from "mongoose";
 
 class dbClient {
 
-    constructor(){
-        const queryString = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.SERVER_DB}/?retryWrites=true&w=majority&appName=adopcion`;
-        this.client = new MongoClient(queryString);
+    constructor(){    
         this.conectarBD();
     }
-
     async conectarBD(){
-       try{
-           await this.client.connect();
-           this.db = this.client.db("adopcion");
-           console.log("Conectado a la BD");
-       }catch(e){
-           console.log(e);
-       } 
+        const queryString = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.SERVER_DB}/?retryWrites=true&w=majority&appName=adopcion`;
+        await mongoose.connect(queryString);
     }
+
+    async cerrarBD(){
+        try{
+            await mongoose.disconnect();
+            console.log("Conexion cerrada");
+        }
+        catch(e){
+            console.error("Error al cerrar la conexion",e);
+        }
+            
+    }
+
 }
 
 export default new dbClient();
--- controllers/mascotas.js
+++ controllers/mascotas.js
@@ -54,6 +54,8 @@ class mascotasController{
     }
 
     
+
+    
 }
 
 export default new mascotasController();
