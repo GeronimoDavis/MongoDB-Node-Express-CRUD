a5c0965f463f2c3b114bc073ea05a60c4fa7aa34
GeronimoDavis <geronimodavis@gmail.com>
2024-12-25 17:18:33 -0300

    Se agrego update y delete

--- controllers/mascotas.js
+++ controllers/mascotas.js
@@ -17,7 +17,7 @@ class mascotasController{
     async getAllMascotas(req, res){
         try{
             const data = await mascotasModel.getAllMascotas();
-            res.status(201).json(data);
+            res.status(200).json(data);
         }catch(e){
            res.status(500).send(e);
         }
@@ -25,7 +25,9 @@ class mascotasController{
 
     async getOneMascota(req, res){
         try{
-            res.status(201).json({status: "getOne-ok"});
+            const { id } = req.params;
+            const data = await mascotasModel.getOneMascotas(id);
+            res.status(200).json(data);
         }catch(e){
            res.status(500).send(e);
         }
@@ -33,7 +35,9 @@ class mascotasController{
     
     async updateMascota(req, res){
         try{
-            res.status(201).json({status: "update-ok"});
+            const { id } = req.params;
+            const data = await mascotasModel.updateMascota(id, req.body);
+            res.status(200).json(data);
         }catch(e){
            res.status(500).send(e);
         }
@@ -41,7 +45,9 @@ class mascotasController{
 
     async deleteMascota(req, res){
         try{
-            res.status(201).json({status: "delete-ok"});
+            const { id } = req.params;
+            const data = await mascotasModel.deleteMascota(id);
+            res.status(200).json(data);
         }catch(e){
            res.status(500).send(e);
         }
--- models/mascotas.js
+++ models/mascotas.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import dbClient from "../config/dbClient.js";
 
 class mascotasModel{
@@ -11,6 +12,21 @@ class mascotasModel{
     async getAllMascotas(){
         const colectionMascotas = dbClient.db.collection("mascotas");
         return await colectionMascotas.find({}).toArray();
+    }
+    
+    async getOneMascotas(id){
+        const colectionMascotas = dbClient.db.collection("mascotas");
+        return await colectionMascotas.findOne({_id: new ObjectId(id)});
+    }
+
+    async updateMascota(id, mascota){
+        const colectionMascota = dbClient.db.collection("mascotas");
+        return await colectionMascota.updateOne({_id: new ObjectId(id)}, {$set: mascota});
+    }
+
+    async deleteMascota(id){
+        const colectionMascota = dbClient.db.collection("mascotas");
+        return await colectionMascota.deleteOne({_id: new ObjectId(id)});
     }
 }
 
