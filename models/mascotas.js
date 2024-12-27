fb8aee20c7c45591a9a01b5acc41b8280708c536
GeronimoDavis <geronimodavis@gmail.com>
2024-12-27 10:11:05 -0300

    Ajustes en modelos

--- models/mascotas.js
+++ models/mascotas.js
@@ -1,32 +1,25 @@
-import { ObjectId } from "mongodb";
-import dbClient from "../config/dbClient.js";
+import Mascota from "../schemas/mascotas.js";
 
 class mascotasModel{
 
     async create(mascota){
-        const colectionMascotas = dbClient.db.collection("mascotas");
-        return await colectionMascotas.insertOne(mascota);
-
+        return await Mascota.create(mascota);
     }
 
     async getAllMascotas(){
-        const colectionMascotas = dbClient.db.collection("mascotas");
-        return await colectionMascotas.find({}).toArray();
+        return await Mascota.find();
     }
     
     async getOneMascotas(id){
-        const colectionMascotas = dbClient.db.collection("mascotas");
-        return await colectionMascotas.findOne({_id: new ObjectId(id)});
+        return await Mascota.findById(id);
     }
 
     async updateMascota(id, mascota){
-        const colectionMascota = dbClient.db.collection("mascotas");
-        return await colectionMascota.updateOne({_id: new ObjectId(id)}, {$set: mascota});
+        return await Mascota.findOneAndUpdate(id, mascota, {new: true});
     }
 
     async deleteMascota(id){
-        const colectionMascota = dbClient.db.collection("mascotas");
-        return await colectionMascota.deleteOne({_id: new ObjectId(id)});
+        return await Mascota.findOneAndDelete(id);
     }
 }
 
