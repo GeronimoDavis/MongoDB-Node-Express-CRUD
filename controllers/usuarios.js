b3866e507a5357fad972d2fc2f4c8eb344572cbd
GeronimoDavis <geronimodavis@gmail.com>
2025-01-08 20:51:11 -0300

    update y delete usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -65,6 +65,26 @@ class usuariosController{
         }
     }
 
+    async updateUsuario(req, res){
+        try{
+            const {id} = req.params;
+            const data = await usuariosModel.updateUsuario(id, req.body);
+            res.status(200).json(data);
+        }catch(e){
+           res.status(500).send(e);
+        }
+    }
+
+    async deleteUsuario(req, res){
+        try{
+            const {id} = req.params;
+            const data = await usuariosModel.deleteUsuario(id);
+            res.status(200).json(data);
+        }catch(e){
+           res.status(500).send(e);
+        }
+    }
+
 }
 
 export default new usuariosController();
